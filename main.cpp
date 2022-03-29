1f22d6e2febab10acdb7756519bc55832a50c984
ksaley <70897569+ksaley@users.noreply.github.com>
2022-03-29 20:31:29 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,10 +14,19 @@ int main() {
     for (int i = 0; i < line_number; ++i) {
         int amount, frequency;
         std::cin >> amount >> frequency;
-        for (int j = 0; j < amount; ++j) {
+        int sum = 0;
+        for (int j = 1; j < amount; ++j) {
             int from, to, time;
             std::cin >> from >> to >> time;
-            edges.emplace_back(edge(from,to,time,frequency));
+            edges.emplace_back(edge(from,to,time,frequency, sum));
+            sum += time;
+        }
+        auto current = --edges.end();
+        sum = 0;
+        for (int j = 1; j < amount; ++j) {
+            (*current).sum_line2 = sum;
+            sum += (*current).m_time;
+            --current;
         }
     }
     Metro metro(stations, edges);
@@ -27,8 +36,9 @@ int main() {
         int from, to, entry_time;
         std::cin >> from >> to >> entry_time;
         auto res =  metro.build_path(from, to,entry_time);
-        for (int j = 0; j < res.size(); ++j) {
-            std::cout << res[j] << " ";
+        for (int j = 0; j < res.second.size(); ++j) {
+            std::cout << res.second[j].id << " ";
         }
+        std::cout << "\nTotal time: " << res.first;
     }
 }

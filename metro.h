acdceb2234a1e344105a36638feab30439114b27
ksaley <70897569+ksaley@users.noreply.github.com>
2022-03-29 20:31:50 +0300

    Update metro.h

--- metro.h
+++ metro.h
@@ -3,24 +3,14 @@
 #include <algorithm>
 #include <utility>
 
-struct station;
-struct metro_line;
-struct metro_line {
-    //station start_station, finish_station;
-    std::string m_name;
-    int frequency;
-};
 
 struct station {
-    std::string m_name;
-    //metro_line* line_name;
+
     int id;
 
     station() = default;
 
     station(int id) : id(id) {}
-
-    station(std::string name, int id) : m_name(std::move(name)), id(id) {}
 
     station(const station &other) = default;
 
@@ -39,49 +29,50 @@ struct station {
 struct edge {
     station m_to, m_from;
     int m_time;
-    int frequency;
-
+    int m_frequency;
+    int sum_line1, sum_line2;
     edge() = default;
 
-    edge(const station &to, const station &from, int time, int frequency) : m_to(to), m_from(from), m_time(time),
-                                                                            frequency(frequency) {}
+    edge(const station &from, const station &to, int time, int frequency) : m_from(from), m_to(to), m_time(time),
+                                                                            m_frequency(frequency) {}
+    edge(const station &from, const station &to, int time, int frequency, int line) : m_from(from), m_to(to), m_time(time),
+                                                                            m_frequency(frequency),sum_line1(line) {}
 };
 
 class Metro {
-    std::vector<station> m_stations; //id - позиция в этом списке??
+    std::vector<station> m_stations;
     std::vector<std::vector<edge>> m_edges;
     const int MAX_TIME = 10'000;
 public:
+    using path = std::pair<long long, std::vector<station>>;
     Metro(std::vector<station> stations, const std::vector<edge> &edges) : m_stations(std::move(stations)) {
-        m_edges.resize(edges.size() + 1);
+        m_edges.resize(m_stations.size() + 1);
         for (const auto &item : edges) {
-            m_edges[item.m_to.id].emplace_back(edge(item.m_from, item.m_to, item.m_time, item.frequency));
-            m_edges[item.m_from.id].emplace_back(edge(item.m_to, item.m_from, item.m_time, item.frequency));
+            m_edges[item.m_from.id].emplace_back(edge(item.m_from, item.m_to, item.m_time, item.m_frequency, item.sum_line1));
+            m_edges[item.m_to.id].emplace_back(edge(item.m_to, item.m_from, item.m_time, item.m_frequency, item.sum_line2));
         }
     }
 
-    std::vector<station> build_path(const station &from, const station &to, int entry_time) {
-        auto res = dijkstra(from, entry_time);
-        res.first[to.id]; // мин длина.
+    path build_path(const station &from, const station &to, int entry_time) {
+        auto res = dijkstra(from, to, entry_time);
         auto parent = res.second;
         int i = to.id;
-        std::vector<station> path;
-        path.push_back(to);
+        std::vector<station> result_path;
+        result_path.push_back(to);
         while (parent[i] != from) {
-            path.push_back(m_stations[parent[i].id]);
+            result_path.push_back(m_stations[parent[i].id]);
             i = parent[i].id;
         }
-        path.push_back(from);
-        std::reverse(path.begin(), path.end());
-        return path;
+        result_path.push_back(from);
+        std::reverse(result_path.begin(), result_path.end());
+        return std::make_pair(res.first, result_path);
     }
 
     std::vector<station> build_reading_path() {
         //to do
-    };
+    }
 private:
-    std::pair<std::vector<long long>, std::vector<station>> dijkstra(const station &start, int entry_time) {
-        //учитывать станции с нулевым переходом???
+    path dijkstra(const station &start, const station &finish, int entry_time) {
         std::vector<long long> dist(m_edges.size(), MAX_TIME);
         std::vector<bool> used(m_edges.size(), false);
         std::vector<int> time_delay(m_stations.size(), MAX_TIME);
@@ -94,10 +85,16 @@ private:
             auto front = *queue.begin();
             queue.erase(*queue.begin());
             for (auto &edge : m_edges[front.second]) {
-                //сколько ждать до след. поезда
-                int time_delay_t = edge.frequency ?
-                                   (edge.frequency - (time_delay[edge.m_from.id] % edge.frequency)) % edge.frequency
-                                                  : 0;
+                int time_delay_t = 0;
+                if (time_delay[edge.m_from.id] < edge.sum_line1) {
+                    time_delay_t += edge.sum_line1 -  time_delay[edge.m_from.id];
+                }
+                else {
+                    time_delay_t = edge.m_frequency ?
+                                   (edge.m_frequency - (time_delay[edge.m_from.id] % edge.m_frequency)) %
+                                   edge.m_frequency
+                                                    : 0;
+                }
                 if (dist[edge.m_to.id] > dist[edge.m_from.id] + edge.m_time + time_delay_t) {
                     if (queue.count({dist[edge.m_to.id], edge.m_to.id})) {
                         queue.erase({dist[edge.m_to.id], edge.m_to.id});
@@ -109,7 +106,7 @@ private:
                 }
             }
         }
-        return std::make_pair(dist, parent);
+        return std::make_pair(dist[finish.id], parent);
     }
 
 };
